f7ab60a5dd59810ab6f1025b22219c61db56401d
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-26 18:33:09 -0700

    small mistake but fixed

--- text_parse.py
+++ text_parse.py
@@ -1,10 +1,9 @@
-# import olivertwist
-
-
+punctuation = ['!', '.', '-', ':', ',', '_', "'"]
 
 text_dict = {
 'character_count': 0,
 'character_count_excluding_spaces': 0,
+'letter_count': 0,
 'line_count': 0,
 'word_count': 0,
 'sentence_count': 0,
@@ -15,11 +14,20 @@ text_dict = {
 
 def parse_text():
     file = open('olivertwist.txt')
-    text = file.read().replace(" ", "").replace('\n','')
+    text = file.read()
     file.close()
-    working_list = list(text)
-    print(working_list)
-    text_dict['character_count'] = len(working_list)
+    strip = text.replace(" ", "").replace('\n','')
+    working_chars = list(strip)
+   
+    text_dict['character_count'] = len(text)
+    text_dict['character_count_excluding_spaces'] = len(list(strip))
+
+    for char in working_chars:
+       if punctuation.count(char) >= 1:
+           strip = strip.replace(char, "")
+
+    text_dict['letter_count'] = len(list(strip))
+    
 
 parse_text()
 print(text_dict)
